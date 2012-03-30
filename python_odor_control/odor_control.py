c73574c0287836685c6d7ac787810c4cd172c9d2
Floris van Breugel <florisvb@gmail.com>
2012-03-30 13:55:47 -0700

    added processing data

--- python_odor_control/odor_control.py
+++ python_odor_control/odor_control.py
@@ -21,6 +21,12 @@ limitations under the License.
 import serial
 import pickle
 
+class OdorData():
+    def __init__(self):
+        pass
+        
+    
+
 class BasicSSR(serial.Serial):
 
     def __init__(self,**kwargs):
@@ -37,7 +43,7 @@ class BasicSSR(serial.Serial):
             data = self.readline()
             print data
             
-            if data == 'done':
+            if 'done' in data:
                 do_work = 0
             else:
                 raw_data.append(data)
@@ -46,15 +52,32 @@ class BasicSSR(serial.Serial):
         
     def process_raw_data(self, raw_data):
         print 'parsing'
-        return
+        
+        data_arr_tmp = np.zeros([len(raw_data), 4])
+        for i, data in enumerate(raw_data):
+            data_split = data.split(',')
+            # Serial << ssr_state << "," << sample_num << "," << time_now << "," << sample_value << endl;
+            for j, d in enumerate(data_arr_tmp):
+                data_arr_tmp[i, j] = data_split[j]
+        
+        odor_data = OdorData()
+        odor_data.odor = data_arr_tmp[:,0]
+        odor_on_index = np.where(np.diff(odor_data.odor)==1)
+        odor_data.odor_on_index = odor_on_index
+        
+        odor_data.sample_num = data_arr_tmp[:,1]
+        odor_data.time = data_arr_tmp[:,2] - data_arr_tmp[odor_data.odor_on_index,2]
+        odor_data.voltage = data_arr_tmp[:,3]
+        
+        return odor_data
         
     def run_experiment(self, pulse):
         
         self.pulse(1, pulse)
         raw_data = listen()
-        processed_data = self.process_raw_data(raw_data)
+        odor_data = self.process_raw_data(raw_data)
         
-        
+        return odor_data
         
         
         
